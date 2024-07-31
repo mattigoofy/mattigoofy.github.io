30ac91cc5cbf9328f1d75584967682b8b2e6d7c3
mattigoofy <159270053+mattigoofy@users.noreply.github.com>
2024-07-31 09:12:14 +0200

    Update script.js

--- taipan/scripts/script.js
+++ taipan/scripts/script.js
@@ -16,18 +16,18 @@ document.getElementById("addButton").addEventListener('click', function(){
     if(document.getElementById("teamSwitch").innerHTML === "Team 1") {
         var points = parseInt(document.getElementById("inputPoints").value);
         scoreTeam1 += points;
-        scoreTeam2 += (100 - points);
+        /*scoreTeam2 += (100 - points);*/
+        
+        scoreTeam1Div = document.getElementById("scoreTeam1");
+        scoreTeam1Div.innerHTML = scoreTeam1Div.innerHTML + "<br>|<br>" + scoreTeam1;
     } else {
         var points = parseInt(document.getElementById("inputPoints").value);
-        scoreTeam1 += (100 - points);
+        /*scoreTeam1 += (100 - points);*/
         scoreTeam2 += points;
+        
+        scoreTeam2Div = document.getElementById("scoreTeam2");
+        scoreTeam2Div.innerHTML = scoreTeam2Div.innerHTML + "<br>|<br>" + scoreTeam2;
     }
-    
-    scoreTeam1Div = document.getElementById("scoreTeam1");
-    scoreTeam2Div = document.getElementById("scoreTeam2");
-    
-    scoreTeam1Div.innerHTML = scoreTeam1Div.innerHTML + "<br>|<br>" + scoreTeam1;
-    scoreTeam2Div.innerHTML = scoreTeam2Div.innerHTML + "<br>|<br>" + scoreTeam2;
     
     document.getElementById("inputPoints").value = "";
 })
